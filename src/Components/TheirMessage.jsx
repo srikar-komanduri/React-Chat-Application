060a007d3f2c1edbf61ceed2ef738a0f4531b331
Srikar Komanduri <komanduri.srikar7@gmail.com>
2021-07-03 23:01:31 +0530

    added our and their messages

--- src/Components/TheirMessage.jsx
+++ src/Components/TheirMessage.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 
-const TheirMessage = () => {
+const TheirMessage = ({lastMessage,message}) => {
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.userName!==message.sender.userName;
     return (
-        <div>
-            
+        <div className="messsage-row">
+            {
+                isFirstMessageByUser && (
+                    <div
+                        className="message-avatar"
+                        style={{backgroundImage:`url(${message?.sender?.avatar})`}}
+                    />
+                )                
+            }
+            {
+                message?.attachments?.length>0 
+                ?(
+                    <img 
+                        src={message.attachments[0].file}
+                        alt="message-attachment"
+                        className="message-image"
+                        style={{margin:isFirstMessageByUser?'4px':'48px'}}
+                    />            
+                )
+                :(
+                    <div className="message" style={{float: 'left', backgroundColor: '#CABCDC' ,margin:isFirstMessageByUser?'4px':'48px'}}> 
+                        {message.text}
+                    </div>
+                )
+            }
         </div>
     )
 }
